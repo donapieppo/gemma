0aa735d57ba02980fd2079d169804b82a3025e70
Donapieppo <donapieppo@yahoo.it>
2020-11-19 12:37:30 +0100

    scale images on save

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,6 +2,30 @@ class Image < ApplicationRecord
   belongs_to :user
   belongs_to :thing, dependent: :destroy
 
-  has_one_attached :photo
-end
+  after_create_commit :photo_format
 
+  has_one_attached :photo
+
+  def resize_photo
+    photo.blob.open do |file|
+      resized = ImageProcessing::MiniMagick.source(file)
+                                           .resize_to_limit!(300, 300)
+      v_filename = photo.filename
+      v_content_type = photo.content_type
+      photo.purge
+      photo.attach(io: resized, filename: v_filename, content_type: v_content_type)
+    end
+  end
+
+  private
+
+  def photo_format
+    return unless photo.attached?
+    if photo.blob.content_type.start_with? 'image/'
+      resize_photo
+    else
+      photo.purge
+      errors.add(:photo, "Non hai inviato un'immagine.")
+    end
+  end
+end
