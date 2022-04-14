a1c60150778e292b6e6e583ffafa3d7c26cddaeb
Pietro <donapieppo@yahoo.it>
2022-04-14 15:49:38 +0200

    dsa

--- app/javascript/controllers/dsa_controller.js
+++ app/javascript/controllers/dsa_controller.js
@@ -9,20 +9,19 @@ export default class extends Controller {
   }
 
   setLastUpn() {
+    console.log('setLastUpn: ' + this.lastupnValue);
     document.getElementById("unload_recipient_upn").value = this.lastupnValue;
   }
 
   searchUser() {
-    console.log("RICERCA: " + this.urlValue);
+    console.log("Modal su url " + this.urlValue);
     const mainModal = document.getElementById('mainModal');
     const bsmainModal = new bootstrap.Modal(mainModal, {});
-    //$(".modal-body").html("<%= j render(template: 'dsausers/popup_find.html.erb') %>");
     mainModal.querySelector(".modal-title").innerHTML = "Ricerca utenti nella Rubrica di Ateneo";
-    mainModal.querySelector(".modal-body").innerHTML = "Ricerca utenti nella Rubrica di Ateneo";
+    bsmainModal.show()
     fetch(this.urlValue)
       .then(response => response.text())
       .then(html => mainModal.querySelector(".modal-body").innerHTML = html)
-    bsmainModal.show()
   }
 
   select_user() {
@@ -33,5 +32,10 @@ export default class extends Controller {
     bsmainModal.hide()
     console.log("SELECT USER FINE")
   }
-};
 
+  submitNameSurnameSearchUser(e) {
+    console.log("submitSearchUser");
+    console.log(e);
+    e.preventDefault;
+  }
+};
