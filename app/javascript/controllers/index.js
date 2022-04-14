64353a19688283b72eb16fb4f96f2ec56e54cb04
Pietro <donapieppo@yahoo.it>
2022-04-14 15:51:43 +0200

    turbo

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,3 +4,5 @@ import { application } from "./application"
 
 import DsaController from "./dsa_controller"
 application.register("dsa", DsaController)
+import DsaAwesomplete from "./dsa_awesomplete.js"
+application.register("dsa-awesomplete", DsaAwesomplete)
