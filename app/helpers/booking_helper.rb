6f6f86cc088ea53495a6fad0c45763d2efd1b7aa
Pietro <donapieppo@yahoo.it>
2022-05-25 15:52:22 +0200

    button_to

--- app/helpers/booking_helper.rb
+++ app/helpers/booking_helper.rb
@@ -7,11 +7,13 @@ module BookingHelper
   end
 
   def link_to_check url
-    link_to dmicon('check'), url, data: {confirm: 'Siete sicuri di volere confermare la prenotazione?'}, title: 'Conferma la prenotazione.' 
+    button_to dmicon('check'), url, form: { data: { 'turbo-confirm': 'Siete sicuri di voler confermare la prenotazione?' }, class: "d-inline px-0 mx-0" }, 
+                                    title: 'Conferma la prenotazione.'
   end
 
   def link_to_change_and_confirm url
-    link_to dmicon('edit'), url, onclick:"return confirm('La prenotazione verrà cancellata e sarà possibile creare uno scarico con i suoi dati.');", title: "Modifica prima di confernare la prenotazione."
+    link_to dmicon('edit'), url, onclick:"return confirm('La prenotazione verrà cancellata e sarà possibile creare uno scarico con i suoi dati.');", 
+                                 title: "Modifica prima di confernare la prenotazione."
   end
 end
 
