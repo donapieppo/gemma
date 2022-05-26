a5050a69c3ef9b7df18a41ee2d1dc10b2b372ed6
Pietro <donapieppo@yahoo.it>
2022-05-26 10:14:50 +0200

    no booking recipe

--- app/helpers/move_helper.rb
+++ app/helpers/move_helper.rb
@@ -6,6 +6,6 @@ module MoveHelper
   def move_action_icons(o, n)
     ((o.is_unload? && n >= -1) ? "" : link_to_edit([:edit, o])) +
     link_to_delete(o) +
-    ((o.is_unload? && o.recipient) ? "<a href='#' title='ricevuta' onClick='window.open(\"#{signing_unload_path(o.id)}\", \"ricevuta\", \"width=700,height=500,scrollbars=no\");'>#{dmicon('file')}</a>".html_safe : "")
+    ((o.is_unload? && ! o.is_booking? && o.recipient) ? "<a href='#' title='ricevuta' onClick='window.open(\"#{signing_unload_path(o.id)}\", \"ricevuta\", \"width=700,height=500,scrollbars=no\");'>#{dmicon('file')}</a>".html_safe : "")
   end
 end
