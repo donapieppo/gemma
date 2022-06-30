f53ece94df4efd0a432a9b39abc3dbe962f446a1
Pietro <donapieppo@yahoo.it>
2022-06-30 11:59:50 +0200

    stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,6 +2,8 @@ import { application } from "./application"
 
 import DsaController from "./dsa_controller"
 application.register("dsa", DsaController)
+import FocusController from "./focus"
+application.register("focus", FocusController)
 import DsaAwesomplete from "./dsa_awesomplete.js"
 application.register("dsa-awesomplete", DsaAwesomplete)
 import { DmTest, TurboModalController } from "dm_unibo_common"
