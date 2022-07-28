85e8c9d02a7aa7223209c3a4f7ca97174afad767
Pietro <donapieppo@yahoo.it>
2022-07-28 11:08:27 +0200

    pagination

---
+++ app/components/pagination/moves_year_component.rb
@@ -0,0 +1,13 @@
+class Pagination::MovesYearComponent < ViewComponent::Base
+  def initialize(first_year, last_year, display_year, thing, deposit)
+    @first_year = first_year
+    @last_year = last_year
+    @display_year = display_year
+    @thing = thing
+    @deposit = deposit
+  end
+
+  def render?
+    @first_year != @last_year
+  end
+end
