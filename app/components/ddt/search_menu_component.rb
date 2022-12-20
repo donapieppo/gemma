02760e7b965fdf5837ed3765c968c8426c3a5bce
Pietro <donapieppo@yahoo.it>
2022-12-20 11:08:02 +0100

    initialize(organization)

--- app/components/ddt/search_menu_component.rb
+++ app/components/ddt/search_menu_component.rb
@@ -1,5 +1,6 @@
 class Ddt::SearchMenuComponent < ViewComponent::Base
-  def initialize
+  def initialize(organization)
+    @organization = organization
     oggi = Date.today.year.to_i
     @years_cia = [oggi, oggi - 1, oggi - 2]
   end
