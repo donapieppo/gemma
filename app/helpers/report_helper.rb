83b344b22cd0050cabf1be5d80102eab961f6e32
Pietro <donapieppo@yahoo.it>
2023-01-26 11:03:04 +0100

    report_layout with title

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -1,5 +1,5 @@
 module ReportHelper
-  def report_layout
+  def report_layout(title: nil)
     content_tag :div, class: 'row' do
       concat (
         content_tag :div, class: 'col-sm-4' do
@@ -8,6 +8,9 @@ module ReportHelper
       ) 
       concat (
         content_tag :div, class: 'col-sm-8' do
+          if title
+            concat(content_tag :h2, title, class: "my-4 text-center")
+          end
           yield
         end
       )
