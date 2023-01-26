b0418518cc64c2679d95ae9ef0953a31189ee54f
Pietro <donapieppo@yahoo.it>
2023-01-26 09:36:21 +0100

    update

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require_relative '../config/environment'
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+# donatini
 require 'support/factory_bot'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -28,8 +29,7 @@ require 'support/factory_bot'
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
+  abort e.to_s.strip
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
