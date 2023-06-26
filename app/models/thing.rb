e8d3f7acd55ad43647a6a917c1fece00ef51f276
Pietro <donapieppo@yahoo.it>
2023-06-26 17:18:34 +0200

    rubocop

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -2,39 +2,39 @@
 class Thing < ApplicationRecord
   belongs_to :group
   belongs_to :organization
-  has_many   :operations
-  has_many   :moves, through: :operations # FIXME
-  has_many   :loads
-  has_many   :unloads
-  has_many   :shifts
-  has_many   :takeovers
-  has_many   :stocks
-  has_many   :deposits,  dependent: :destroy
-  has_many   :locations, through:   :deposits
-  has_many   :bookings,  dependent: :destroy
-  has_many   :barcodes,  dependent: :destroy
-  has_many   :images,    dependent: :destroy
+  has_many :operations
+  has_many :moves, through: :operations # FIXME
+  has_many :loads
+  has_many :unloads
+  has_many :shifts
+  has_many :takeovers
+  has_many :stocks
+  has_many :deposits, dependent: :destroy
+  has_many :locations, through: :deposits
+  has_many :bookings, dependent: :destroy
+  has_many :barcodes, dependent: :destroy
+  has_many :images, dependent: :destroy
 
-  serialize  :future_prices, Array
+  serialize :future_prices, Array
 
   before_validation :strip_name_blanks
 
   validates :organization_id, :group_id, presence: true
-  validates :name, uniqueness: { scope: [:organization_id], message: "L'articolo esiste già.", case_sensitive: false },
+  validates :name, uniqueness: {scope: [:organization_id], message: "L'articolo esiste già.", case_sensitive: false},
                    presence: true
-  validates :minimum, numericality: { greater_than_or_equal_to: 0 } 
+  validates :minimum, numericality: {greater_than_or_equal_to: 0}
 
-  validate  :check_group_organization  
+  validate :check_group_organization
 
   before_destroy :check_no_associated_moves, prepend: true
 
   def create_deposits(location_ids)
     location_ids = location_ids.select { |i| i.to_i > 0 }
     Location.find(location_ids).each do |loc|
-      (loc.organization_id == self.organization_id) or raise DmUniboCommon::MismatchOrganization, 'Struttura non permessa.'
-      loc.deposits.create(thing_id:        self.id, 
+      (loc.organization_id == self.organization_id) or raise DmUniboCommon::MismatchOrganization, "Struttura non permessa."
+      loc.deposits.create(thing_id: self.id,
                           organization_id: self.organization_id,
-                          actual:          0) or return false
+                          actual: 0) or return false
     end
   end
 
@@ -54,7 +54,7 @@ class Thing < ApplicationRecord
     # Piu' veloce con deposits ma per ora va bene cosi'
     # sum = self.deposits.sum(:actual)
     sum = self.operations.sum(:number)
-    ( sum < 0 ) and raise Gemma::NegativeDeposit
+    (sum < 0) and raise Gemma::NegativeDeposit
     self.update_attribute(:total, sum)
   end
 
@@ -63,7 +63,7 @@ class Thing < ApplicationRecord
   end
 
   def to_s_with_description
-    d = self.description.blank? ? '' : " (#{self.description})"
+    d = self.description.blank? ? "" : " (#{self.description})"
     self.name + d
   end
 
@@ -85,16 +85,16 @@ class Thing < ApplicationRecord
         while number > 0
           (stack_operation, n, p) = pc.get(number)
 
-          stack_price = n*p
+          stack_price = n * p
 
           if stack_price > 0
             if stack_operation.is_a?(Load)
               ddt = stack_operation.ddt
-              price_operations << { id: stack_operation.id, n: n, p: stack_price, desc: "#{ddt.gen} n. #{ddt.number}" }
+              price_operations << {id: stack_operation.id, n: n, p: stack_price, desc: "#{ddt.gen} n. #{ddt.number}"}
             elsif stack_operation.is_a?(Takeover)
-              price_operations << { id: stack_operation.id, n: n, p: stack_price, desc: "reso da #{stack_operation.recipient.upn}" }
+              price_operations << {id: stack_operation.id, n: n, p: stack_price, desc: "reso da #{stack_operation.recipient.upn}"}
             elsif stack_operation.is_a?(Stock)
-              price_operations << { id: stack_operation.id, n: n, p: stack_price, desc: 'giacenza iniziale' }
+              price_operations << {id: stack_operation.id, n: n, p: stack_price, desc: "giacenza iniziale"}
             end
           end
 
@@ -119,13 +119,13 @@ class Thing < ApplicationRecord
   end
 
   def check_group_organization
-    (self.group.organization_id == self.organization_id) or raise DmUniboCommon::MismatchOrganization, 'Struttura non permessa.'
+    (self.group.organization_id == self.organization_id) or raise DmUniboCommon::MismatchOrganization, "Struttura non permessa."
   end
 
   def check_no_associated_moves
     # FIXME: BUG risultano certe operations senza moves... da riflettere e sistemare
     if self.moves.any? || self.operations.any?
-      self.errors.add(:base, 'Ci sono carichi e scarichi associati a questo articolo che devono prima essere cancellati.')
+      self.errors.add(:base, "Ci sono carichi e scarichi associati a questo articolo che devono prima essere cancellati.")
       throw :abort
     end
   end
