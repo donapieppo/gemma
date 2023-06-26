9169231219fcc5145d0af047b2eac0b01177f435
Pietro <donapieppo@yahoo.it>
2023-06-26 17:21:11 +0200

    thing_color_class

--- app/helpers/thing_helper.rb
+++ app/helpers/thing_helper.rb
@@ -1,22 +1,32 @@
 module ThingHelper
   def thing_actions_enabled?
-    policy(current_organization).book? || policy(current_organization).unload? 
+    policy(current_organization).book? || policy(current_organization).unload?
   end
 
   # cliccabile se ci sono oggetti o se si ha diritto di fare qualcosa
   def thing_actions_url(thing)
     if thing_actions_enabled?
-      (policy(current_organization).book? && ! policy(current_organization).unload?) ? new_thing_booking_path(thing) : new_thing_unload_path(thing) 
+      (policy(current_organization).book? && !policy(current_organization).unload?) ? new_thing_booking_path(thing) : new_thing_unload_path(thing)
     else
-      '#'
+      "#"
     end
   end
 
   def thing_image(thing)
     if thing && (i = thing.images.first) && i.photo.attached?
-      content_tag :div, class: 'image centered my-3' do
+      content_tag :div, class: "image centered my-3" do
         image_tag i.photo
       end
+    end
+  end
+
+  def thing_color_class(thing, number)
+    if number == 0
+      "empty"
+    elsif number < thing.minimum
+      "warning"
+    else
+      "full"
     end
   end
 end
