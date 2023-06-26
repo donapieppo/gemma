950bbc220b3aea9ddc5adea3874ea2d41e4b2b2c
Pietro <donapieppo@yahoo.it>
2023-06-26 17:19:34 +0200

    rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < DmUniboCommon::ApplicationController
   # not session booking
   # and have no authorization in current_organization
   def after_current_user_and_organization
-    if current_organization 
-      if session[:booking] != current_organization.id && current_organization.booking && (! policy(current_organization).unload?)
+    if current_organization
+      if session[:booking] != current_organization.id && current_organization.booking && (!policy(current_organization).unload?)
         redirect_to current_organization_booking_accept_path
       end
     elsif current_user
@@ -21,30 +21,28 @@ class ApplicationController < DmUniboCommon::ApplicationController
   end
 
   def send_report
-    @title ||= ''
+    @title ||= ""
     rep = GemmaReport.new
-    rep.title        = @title
+    rep.title = @title
     rep.organization = current_organization
-    rep.separator    = @separator
+    rep.separator = @separator
     rep.separator_page_break = @separator_page_break
     rep.separator_first_line = @separator_first_line
     rep.separator_first_line_field = @separator_first_line_field
-    filename = @title.downcase.gsub(' ', '_') + ".pdf"
-    send_data rep.to_pdf(@res, @fields), filename: filename, 
-                                         type: "application/pdf"
+    filename = @title.downcase.tr(" ", "_") + ".pdf"
+    send_data rep.to_pdf(@res, @fields), filename: filename, type: "application/pdf"
   end
 
   def fix_prices(par, with_iva)
-    if par[:price_int] and par[:price_dec]
+    if par[:price_int] && par[:price_dec]
       par[:price_int] = par[:price_int].to_i
       par[:price_dec] = par[:price_dec].to_i
-      if with_iva == 'n'
+      if with_iva == "n"
         price = par[:price_int] * 100 + par[:price_dec] # in centesimi
         price *= IVA
-        par[:price_int] = (price/100).to_i
-        par[:price_dec] = (price - ((price/100).to_i * 100)).to_i
+        par[:price_int] = (price / 100).to_i
+        par[:price_dec] = (price - ((price / 100).to_i * 100)).to_i
       end
     end
   end
 end
-
--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,14 +5,14 @@ class BookingsController < ApplicationController
   def index
     authorize Booking
     if policy(current_organization).give?
-      @books = current_organization.bookings.includes(:user, :recipient, :thing).order('users.surname, date') 
-      if params[:user_id] 
-        @user  = User.find(params[:user_id])
+      @books = current_organization.bookings.includes(:user, :recipient, :thing).order("users.surname, date")
+      if params[:user_id]
+        @user = User.find(params[:user_id])
         @books = @books.where(user: @user)
-      elsif params[:thing_id] 
+      elsif params[:thing_id]
         @thing = Thing.find(params[:thing_id])
         @books = @books.where(thing: @thing)
-      elsif params[:barcode] 
+      elsif params[:barcode]
         @barcode = current_organization.barcodes.includes(:thing).where(name: params[:barcode]).first
         @thing = @barcode.thing if @barcode
         @books = @barcode ? @books.where(thing_id: @barcode.thing_id) : []
@@ -25,10 +25,10 @@ class BookingsController < ApplicationController
     @books = @books.to_a
 
     render :mylist unless policy(current_organization).give?
-  end 
+  end
 
   def new
-    if @thing.total == 0 
+    if @thing.total == 0
       skip_authorization
       redirect_to group_things_path(@thing.group_id), alert: "Non sono presenti - #{@thing} - da scaricare."
     else
@@ -39,11 +39,11 @@ class BookingsController < ApplicationController
   end
 
   def create
-    params[:booking][:numbers] = { params[:booking].delete(:deposit_id) => params[:booking].delete(:number).to_i * -1 }
+    params[:booking][:numbers] = {params[:booking].delete(:deposit_id) => params[:booking].delete(:number).to_i * -1}
 
-    @book = @thing.bookings.new(booking_params) 
+    @book = @thing.bookings.new(booking_params)
 
-    @book.organization_id = current_organization.id 
+    @book.organization_id = current_organization.id
     @book.user_id = current_user.id
     @book.date = Date.today
 
@@ -51,12 +51,12 @@ class BookingsController < ApplicationController
     begin
       res = @book.save
     rescue Gemma::NegativeDeposit => e
-       @book.errors.add(:base, e.to_s)
-       res = false
+      @book.errors.add(:base, e.to_s)
+      res = false
     end
 
-    if res 
-      redirect_to bookings_path(highlight: @book), notice: 'Prenotazione effettuata correttamente.'
+    if res
+      redirect_to bookings_path(highlight: @book), notice: "Prenotazione effettuata correttamente."
     else
       @book.number = @book.number * -1
       @delegators = current_user.get_delegators(current_organization.id).to_a.push(current_user)
@@ -73,36 +73,36 @@ class BookingsController < ApplicationController
     # deleghe. Se non delega nessuno l'operatore scarica per l'utente stesso
     # se c'e' delega il delegato sparisce e l'operatore fa lo scarico.
     # @unload.recipient_id = @book.user_id unless @book.recipient_id
-    h = { recipient_id: (@book.recipient_id or @book.user_id), 
-          date:    @book.date, # try to keep because of prices different in different times
-          number:  @book.number * -1 }
+    h = {recipient_id: (@book.recipient_id or @book.user_id),
+         date: @book.date, # try to keep because of prices different in different times
+         number: @book.number * -1}
     @book.destroy
 
     # can't create before @boo destroy or checks app/model/operation fails
     @unload = @thing.unloads.new(h)
 
-    render 'unloads/new'
+    render "unloads/new"
   end
 
   def destroy
-    if @book.destroy 
-      flash[:notice] = 'La prenotazione è stata cancellata.'
-    else 
-      flash[:error] = 'Errore sulla cancellazione.'
+    if @book.destroy
+      flash[:notice] = "La prenotazione è stata cancellata."
+    else
+      flash[:error] = "Errore sulla cancellazione."
     end
     redirect_to bookings_path
   end
 
   def confirm
     if @book.confirm
-      flash[:notice] = 'La prenotazione è stata confermata.'
-    else 
-      flash[:error]  = 'Errore sulla conferma dello scarico.'
+      flash[:notice] = "La prenotazione è stata confermata."
+    else
+      flash[:error] = "Errore sulla conferma dello scarico."
     end
     redirect_to bookings_path
   end
 
-  private 
+  private
 
   def booking_params
     params[:booking].permit(:note, :recipient_id, numbers: params[:booking][:numbers].try(:keys))
