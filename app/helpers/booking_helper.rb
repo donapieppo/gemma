0d5c3d8494fadf151e6c9c06f716cb486e6fa9ff
Pietro <donapieppo@yahoo.it>
2023-07-05 11:25:40 +0200

    rubocop

--- app/helpers/booking_helper.rb
+++ app/helpers/booking_helper.rb
@@ -1,19 +1,24 @@
 module BookingHelper
   def booking_title
     u = @user ? "di #{h @user}" : ""
-    t = @thing? "per #{h @thing}" : ""
+    t = @thing ? "per #{h @thing}" : ""
 
     "Prenotazioni <span class='font-weight-normal'>#{u} #{t}</span>".html_safe
   end
 
   def link_to_check url
-    button_to dmicon('check'), url, form: { data: { 'turbo-confirm': 'Siete sicuri di voler confermare la prenotazione?' }, class: "d-inline px-0 mx-0" }, 
-                                    title: 'Conferma la prenotazione.'
+    button_to url, form: {
+      data: {"turbo-confirm": "Siete sicuri di voler confermare la prenotazione?"},
+      class: "d-inline px-0 mx-0"
+    }, title: "Conferma la prenotazione." do
+      dm_icon "circle-check", "regular"
+    end
   end
 
   def link_to_change_and_confirm url
-    link_to dmicon('edit'), url, onclick:"return confirm('La prenotazione verrà cancellata e sarà possibile creare uno scarico con i suoi dati.');", 
-                                 title: "Modifica prima di confernare la prenotazione."
+    link_to dm_icon("edit"),
+      url,
+      onclick: "return confirm('La prenotazione verrà cancellata e sarà possibile creare uno scarico con i suoi dati.');",
+      title: "Modifica prima di confernare la prenotazione."
   end
 end
-
