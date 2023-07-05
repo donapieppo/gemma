c30b9134bec61fd46275ee01a74e9a9567ce7fb8
Pietro <donapieppo@yahoo.it>
2023-07-05 11:09:54 +0200

    REFACTOR

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -1,32 +1,39 @@
 class MovesController < ApplicationController
-  # se chiediamo un certo deposito vediamo tutte le operazioni con i moves di quel deposito
   def index
-    authorize Move
+    skip_authorization
 
     if params[:thing_id]
-      @thing = current_organization.things.find(params[:thing_id]) 
-    else 
+      @thing = current_organization.things.find(params[:thing_id])
+    else
       @deposit = current_organization.deposits.includes(:location).find(params[:deposit_id])
       @thing = @deposit.thing
     end
 
-    @operations = @thing.operations.ordered.includes(:user, :recipient, ddt: :supplier)
-    if @deposit
-      @operations = @operations.ordered.includes(:user, :recipient, :moves)
-                               .where('moves.deposit_id = ?', @deposit.id).references(:moves) 
+    @moves = Move
+      .joins(:operation)
+      .where(operation: {thing_id: @thing.id})
+      .includes(deposit: :location, operation: [:user, :recipient, ddt: :supplier])
+      .order("operation.date ASC, operation.number DESC")
+
+    @moves = if @deposit
+      @moves.where(deposit_id: @deposit.id)
+    else
+      @moves.group(:operation_id)
     end
 
-    if @operations.empty?
-      redirect_to thing_path(@thing), alert: 'Non sono ancora stati registrati movimenti per il materiale selezionato.'
-    else
+    @moves.load
+
+    if @moves.any?
       @deposits = @thing.deposits
 
-      @first_year = @operations.first.date.year
-      @last_year = @operations.last.date.year
+      @first_year = @moves.first.operation.date.year
+      @last_year = @moves.last.operation.date.year
       @display_year = (params[:y] || @last_year).to_i
       if @display_year < @first_year || @display_year > @last_year
         @display_year = @last_year
       end
+    else
+      redirect_to thing_path(@thing), alert: "Non sono ancora stati registrati movimenti per il materiale selezionato."
     end
   end
 end
