21d7fe960bafa9befa4d6eda04ad07b4f46acdf4
Pietro <donapieppo@yahoo.it>
2024-01-15 12:05:47 +0100

    rubocop

--- app/components/dsa_awesomplete_component.rb
+++ app/components/dsa_awesomplete_component.rb
@@ -2,10 +2,9 @@
 
 # FIXME maybe singleton in case of nultiple user because of cache
 class DsaAwesompleteComponent < ViewComponent::Base
-  def initialize(current_organization_id, form, what, hint: '')
+  def initialize(current_organization_id, form, what, hint: "")
     @form = form
     @what = what
-    @cache_users_json = User.all_in_cache(current_organization_id).map{|x| "#{x.to_s} (#{x.upn})"}.to_json
+    @cache_users_json = User.all_in_cache(current_organization_id).map { |x| "#{x.to_s} (#{x.upn})" }.to_json
   end
 end
-
