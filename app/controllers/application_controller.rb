29273ae9eb422e062f3877b0a08016d5d1d5d873
Pietro <donapieppo@yahoo.it>
2024-05-01 12:17:57 +0200

    clear after action present in dm_unibo_common

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < DmUniboCommon::ApplicationController
   before_action :set_current_user, :update_authorization, :set_current_organization, :after_current_user_and_organization, :log_current_user, :force_sso_user
-  after_action :verify_authorized, except: [:who_impersonate, :impersonate, :stop_impersonating]
 
   # formally accept affiliation if
   # not session booking
