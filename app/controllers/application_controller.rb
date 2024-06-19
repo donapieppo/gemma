d550b592c6edb67f152aab86613da01d2918ac40
Pietro Donatini <pietro.donatini@unibo.it>
2024-06-19 10:06:09 +0200

    moved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,18 +39,4 @@ class ApplicationController < DmUniboCommon::ApplicationController
     filename = @title.downcase.tr(" ", "_") + ".pdf"
     send_data rep.to_pdf(@res, @fields), filename: filename, type: "application/pdf"
   end
-
-  def fix_prices(p, add_iva)
-    p[:price_int] = BigDecimal(p[:price_int])
-    p[:price_dec] = BigDecimal(p[:price_dec])
-    add_iva = BigDecimal(add_iva)
-
-    price = (p[:price_int] * 100 + p[:price_dec]).to_f
-    if price > 0 && add_iva > 0
-      price *= ((100 + add_iva) / 100)
-      p[:price_int] = (price / 100).to_i
-      p[:price_dec] = (price - ((price / 100).to_i * 100)).to_i
-    end
-    p
-  end
 end
