c8b7c9696beac58d3497a71065e4cf3c000010bc
Pietro Donatini <pietro.donatini@unibo.it>
2024-06-21 09:01:12 +0200

    home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,8 @@ class HomeController < ApplicationController
     skip_authorization
     if current_user&.current_organization
       redirect_to groups_path(__org__: current_user.current_organization.code) and return
+    elsif current_user
+      @available_organizations = current_user.my_organizations(ordering: :code)
     end
   end
 end
