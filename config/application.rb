b5b5ea4ed6165abd231953a97a968e7224771e61
Pietro Donatini <pietro.donatini@unibo.it>
2024-06-21 13:02:26 +0200

    app

---
+++ config/application.rb
@@ -0,0 +1,31 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Gemma
+  class Application < Rails::Application
+    config.load_defaults 7.1
+
+    config.hosts << "gemma.unibo.it"
+
+    config.autoload_paths << "#{Rails.root}/app/pdfs"
+    config.time_zone = "Rome"
+    config.i18n.default_locale = :it
+
+    config.authlevels = {
+      read: 10,
+      book: 15,
+      unload: 20,
+      give: 30,
+      manage: 40,
+      edit: 50
+    }
+
+    config.dm_unibo_common = ActiveSupport::HashWithIndifferentAccess.new config_for(:dm_unibo_common)
+    config.active_record.yaml_column_permitted_classes = [Symbol]
+  end
+end
