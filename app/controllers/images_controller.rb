25528705f40af434f69bce3b4aff6852d6066cf6
Pietro Donatini <pietro.donatini@unibo.it>
2024-06-21 09:05:35 +0200

    resize image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,9 @@
 class ImagesController < ApplicationController
+  before_action :resize_image, only: [:create]
+
   def index
     authorize Image
-    @images = Image.includes(thing: :organization).order('things.organization_id')
+    @images = Image.includes(thing: :organization).order("things.organization_id")
   end
 
   def create
@@ -23,6 +25,7 @@ class ImagesController < ApplicationController
   def destroy
     @image = Image.find(params[:id])
     authorize @image
+    @image.photo.purge
     @image.delete
     redirect_to edit_thing_path(@image.thing_id)
   end
@@ -31,5 +34,14 @@ class ImagesController < ApplicationController
 
   def image_params
     params[:image].permit(:photo)
+  end
+
+  def resize_image
+    if params[:image][:photo]
+      path = params[:image][:photo].tempfile.path
+      image = ImageProcessing::Vips.source(path)
+      result = image.resize_to_limit!(600, 600)
+      params[:image][:photo].tempfile = result
+    end
   end
 end
