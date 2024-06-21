516389d35c1c284e83a355c26cf8f11f8348dde5
Pietro Donatini <pietro.donatini@unibo.it>
2024-06-21 09:06:06 +0200

    PriceMethods

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,31 +1,33 @@
 class StocksController < ApplicationController
+  include PriceMethods
+
   before_action :set_thing, only: [:new, :create]
   before_action :set_stock_and_thing_and_check_permission, only: [:edit, :update, :destroy]
 
   def new
-    @thing.has_operation? and raise 'Non si può avere giacenza inizale. Ci sono altri carichi'
-    @stock   = @thing.stocks.new(date: Date.today, organization_id: current_organization.id)
+    @thing.has_operation? and raise "Non si può avere giacenza inizale. Ci sono altri carichi"
+    @stock = @thing.stocks.new(date: Date.today, organization_id: current_organization.id)
     authorize @stock
     @numbers = {}
   end
 
   def create
-    @thing.has_operation? and raise 'Non si può inserire una giacenza inizale in quanto sono presenti altri carichi'
+    @thing.has_operation? and raise "Non si può inserire una giacenza inizale in quanto sono presenti altri carichi"
 
     params[:stock][:numbers] = @numbers = params[:numbers]
-    fix_prices(params[:stock], params['price_with_iva'])
+    fix_prices(params[:stock], params["price_add_iva"])
 
     @stock = @thing.stocks.new(stock_params)
 
     # @stock.numbers         = @numbers
     @stock.organization_id = current_organization.id
-    @stock.user_id         = current_user.id
+    @stock.user_id = current_user.id
 
     authorize @stock
 
     # il create non vede raisare perche' sempre history_coherent
     if @stock.save
-      redirect_to group_things_path(@stock.thing.group_id), notice: 'La giacenza iniziale è stata registrata correttamente.'
+      redirect_to group_things_path(@stock.thing.group_id), notice: "La giacenza iniziale è stata registrata correttamente."
     else
       render action: :new, status: :unprocessable_entity
     end
@@ -44,11 +46,11 @@ class StocksController < ApplicationController
   end
 
   def update
-    fix_prices(params[:stock], params['price_with_iva'])
+    fix_prices(params[:stock], params["price_add_iva"])
 
     params[:stock][:numbers] = params[:numbers].permit!
 
-    begin 
+    begin
       res = @stock.aggiorna(stock_params)
     rescue Gemma::NegativeDeposit => e
       @stock.errors.add(:base, e.to_s)
@@ -56,7 +58,7 @@ class StocksController < ApplicationController
     end
 
     if res
-      redirect_to thing_moves_path(@stock.thing_id), notice: 'La giacenza iniziale è stata correttamente aggiornata.'
+      redirect_to thing_moves_path(@stock.thing_id), notice: "La giacenza iniziale è stata correttamente aggiornata."
     else
       @numbers = @stock.moves.each_with_object({}) { |move, numbers| numbers[move.deposit_id] = move.number }
       render action: :new, status: :unprocessable_entity
@@ -71,8 +73,8 @@ class StocksController < ApplicationController
       res = false
     end
 
-    if res 
-      flash[:notice] = 'La giacenza iniziale è stata eliminata.'
+    if res
+      flash[:notice] = "La giacenza iniziale è stata eliminata."
     else
       flash[:alert] ||= "Non è stato possibile eliminare la giacenza iniziale. #{@stock.errors.on_base}."
     end
