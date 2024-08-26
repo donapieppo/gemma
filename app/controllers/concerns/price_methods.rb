9031bfa134eff3b62398fe5d84ea2ba76a4a7d1b
Pietro Donatini <pietro.donatini@unibo.it>
2024-08-26 12:20:51 +0200

    bug price nil

--- app/controllers/concerns/price_methods.rb
+++ app/controllers/concerns/price_methods.rb
@@ -2,15 +2,17 @@ module PriceMethods
   extend ActiveSupport::Concern
 
   def fix_prices(p, add_iva)
-    p[:price_int] = BigDecimal(p[:price_int])
-    p[:price_dec] = BigDecimal(p[:price_dec])
-    add_iva = BigDecimal(add_iva)
+    if p[:price_int] && p[:price_dec]
+      p[:price_int] = BigDecimal(p[:price_int])
+      p[:price_dec] = BigDecimal(p[:price_dec])
+      add_iva = BigDecimal(add_iva)
 
-    price = (p[:price_int] * 100 + p[:price_dec])
-    if price > 0 && add_iva > 0
-      price *= ((100 + add_iva) / 100)
-      p[:price_int] = (price / 100).to_i
-      p[:price_dec] = (price - ((price / 100).to_i * 100)).to_i
+      price = (p[:price_int] * 100 + p[:price_dec])
+      if price > 0 && add_iva > 0
+        price *= ((100 + add_iva) / 100)
+        p[:price_int] = (price / 100).to_i
+        p[:price_dec] = (price - ((price / 100).to_i * 100)).to_i
+      end
     end
     p
   end
