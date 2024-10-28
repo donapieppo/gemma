35b01b45a1fa58aef904bd6e0e2ea5f7de95bc36
Pietro Donatini <pietro.donatini@unibo.it>
2024-10-28 08:46:01 +0100

    sleep

--- app/inputs/unload_location_input.rb
+++ app/inputs/unload_location_input.rb
@@ -13,4 +13,4 @@ end
 # o = object.thing.deposits.map {|d| ["#{d.location} (#{d.actual} disponibili)".html_safe, d.id]}
 # dis = object.thing.deposits.map {|d| d.actual < 1 }
 # @builder.input :deposit_id, collection: o , disabled: dis, as: :radio_buttons, label: "Scegliere la provenienza"
-# template.collection_radio_buttons(:unload, :deposit_id, o, :last, :first) #{ disabled: (dep.actual < 1) }) 
+# template.collection_radio_buttons(:unload, :deposit_id, o, :last, :first) #{ disabled: (dep.actual < 1) })
