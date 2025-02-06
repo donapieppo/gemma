9584f36834591112b82b75d4be430b290caa6189
Pietro Donatini <pietro.donatini@unibo.it>
2025-02-06 13:40:00 +0100

    rubocop

--- app/models/arch_thing.rb
+++ app/models/arch_thing.rb
@@ -1,8 +1,7 @@
 # Archivio 
 class ArchThing < ApplicationRecord
   belongs_to :organization
-  has_many   :operations, class_name: 'ArchOperation', dependent: :destroy, foreign_key: :thing_id
+  has_many :operations, class_name: "ArchOperation", dependent: :destroy, foreign_key: :thing_id
 
-  scope :from_new, -> t { where(oldid: t.id) }
+  scope :from_new, ->(t) { where(oldid: t.id) }
 end
-
