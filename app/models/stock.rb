8df6ab0a5e37c3a6c031f1f67cae0ffeed59240e
Pietro Donatini <pietro.donatini@unibo.it>
2025-05-09 12:20:25 +0200

    validate number > 0

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,29 +2,30 @@
 class Stock < Operation
   belongs_to :thing
   belongs_to :organization
-  has_many   :moves, foreign_key: :operation_id
+  has_many :moves, foreign_key: :operation_id
 
   validate :no_previous_stock
+  validates :number, numericality: {greater_than: 0}
 
-  after_save    :recalculate_prices
+  after_save :recalculate_prices
   after_destroy :recalculate_prices
 
   def initialize(attributes = nil)
     if attributes.is_a?(Hash)
       attributes[:ddt_id] = nil
-      attributes[:ycia]   = nil
-      attributes[:ncia]   = nil
+      attributes[:ycia] = nil
+      attributes[:ncia] = nil
     end
-    super(attributes)
+    super
   end
 
   # verifico che non ci siano precedenti giacenze iniziali per lo stesso oggetto
   def no_previous_stock
-    if (m = Stock.where(thing_id: self.thing_id).first)
+    if (m = Stock.where(thing_id: thing_id).first)
       # se non sono nuovo o se sono nuovo e non solo l'unico
-      if self.new_record? || m.id != self.id
-        errors.add(:base, 'Esiste già una giacenza iniziale per questo materiale.')
-        throw(:abort) 
+      if new_record? || m.id != id
+        errors.add(:base, "Esiste già una giacenza iniziale per questo materiale.")
+        throw(:abort)
       end
     end
     true
