8f583f52d88f48f1abc4291db5fb0153e498ba7c
Pietro Donatini <pietro.donatini@unibo.it>
2025-05-09 17:08:28 +0200

    cache for User.all_in_cache in view

--- app/components/dsa_awesomplete_component.rb
+++ app/components/dsa_awesomplete_component.rb
@@ -2,9 +2,21 @@
 
 # FIXME maybe singleton in case of nultiple user because of cache
 class DsaAwesompleteComponent < ViewComponent::Base
+  @@organization_users_cache = {}
+  @@organization_users_cache_start = 0
+
   def initialize(current_organization_id, form, what, hint: "")
     @form = form
     @what = what
-    @cache_users_json = User.all_in_cache(current_organization_id).map { |x| "#{x} (#{x.upn})" }.to_json
+
+    if (Time.now - @@organization_users_cache_start) > 300
+      @@organization_users_cache = {}
+      @@organization_users_cache_start = Time.now
+    end
+    if !@@organization_users_cache.key?(current_organization_id)
+      @@organization_users_cache[current_organization_id] ||= User.all_in_cache(current_organization_id).map { |x| "#{x} (#{x.upn})" }.to_json
+    end
+
+    @cache_users_json = @@organization_users_cache[current_organization_id]
   end
 end
