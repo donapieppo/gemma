7088592736181c35d874cc0f1e5c1d6dd66d1a77
Pietro Donatini <pietro.donatini@unibo.it>
2025-05-09 17:09:44 +0200

    rubocop

--- app/models/price_calculator.rb
+++ app/models/price_calculator.rb
@@ -4,7 +4,7 @@ class PriceCalculator
   end
 
   def add(o)
-    @stack_of_loads << { operation: o, number: o.number, price: o.price.to_f / o.number }
+    @stack_of_loads << {operation: o, number: o.number, price: o.price.to_f / o.number}
   end
 
   # gets number available and single item price from FIRST in @stack_of_loads
@@ -22,7 +22,7 @@ class PriceCalculator
   end
 
   def remaining_stack
-    @stack_of_loads.map { |s| { id: s[:operation].id, n: s[:number], p: s[:price] } }
+    @stack_of_loads.map { |s| {id: s[:operation].id, n: s[:number], p: s[:price]} }
   end
 
   # valore di quello che resta in magazzino
