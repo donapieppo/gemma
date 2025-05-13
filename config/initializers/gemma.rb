b4cab1e8006b2713597bbd33b53929209d43669b
Pietro Donatini <pietro.donatini@unibo.it>
2025-05-13 15:02:51 +0200

    dewar_liters_and_hot_liters

--- config/initializers/gemma.rb
+++ config/initializers/gemma.rb
@@ -14,5 +14,21 @@ module Gemma
     config.session_store :cookie_store, key: "_gemma25"
 
     config.active_record.belongs_to_required_by_default = false
+
+    # Per azoto: serialize :containers, coder: YAML
+    # nel caso di container caldo l'azoto usato e' x * config.containers_with_multipliers[x]
+    config.dewar_liters_and_hot_liters = {
+      3 => 5,
+      5 => 8,
+      10 => 15,
+      25 => 35,
+      30 => 41,
+      35 => 47,
+      50 => 65,
+      80 => 100,
+      100 => 120,
+      120 => 144
+    }
+    config.possible_dewars = config.dewar_liters_and_hot_liters.keys
   end
 end
