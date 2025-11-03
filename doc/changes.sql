510de822fb237debb86ac58b325005c8532fd6c9
Pietro Donatini <pietro.donatini@unibo.it>
2025-11-03 14:44:54 +0100

    changes

--- doc/changes.sql
+++ doc/changes.sql
@@ -1,2 +1,14 @@
-ALTER TABLE things ADD COLUMN dewars text;
+-- ALTER TABLE things ADD COLUMN dewars text;
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
+    description TEXT
+);
 
+ALTER TABLE operations
+ADD COLUMN department_id INT,
+ADD CONSTRAINT fk_operation_department
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE;
