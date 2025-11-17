f4ce2ae5be66d763605e0f0607a4fb1502b201f3
Pietro Donatini <pietro.donatini@unibo.it>
2025-11-17 09:16:42 +0100

    picking_point

--- doc/changes.sql
+++ doc/changes.sql
@@ -1,40 +1,39 @@
 -- ALTER TABLE things ADD COLUMN dewars text;
-CREATE TABLE `destinations` (
+CREATE TABLE `picking_points` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `organization_id` int(11) unsigned NOT NULL,
-  `building` varchar(255) NOT NULL,
-  `detail` varchar(255),
+  `name` varchar(255) NOT NULL,
   `description` text DEFAULT NULL,
   PRIMARY KEY (`id`)
-  -- KEY `organization_id` (`organization_id`),
-  -- CONSTRAINT `buildings_ibfk_1` FOREIGN KEY (`organization_id`) REFERENCES `organizations` (`id`)
+  KEY `organization_id` (`organization_id`),
+  CONSTRAINT `fk_picking_points_organizations` FOREIGN KEY (`organization_id`) REFERENCES `organizations` (`id`)
 );
 
-insert into `destinations` values (0, 255, 'UE5', 'P0', '');
-insert into `destinations` values (0, 255, 'UE5', 'P1', '');
-insert into `destinations` values (0, 255, 'UE5', 'P2', '');
-insert into `destinations` values (0, 255, 'UE5', 'P3', '');
-insert into `destinations` values (0, 255, 'UE5', 'P4', '');
-insert into `destinations` values (0, 255, 'UE5', 'P5', '');
-insert into `destinations` values (0, 255, 'UE5', 'P6', '');
-insert into `destinations` values (0, 255, 'UE5', 'P7', '');
-insert into `destinations` values (0, 255, 'UE4', 'Corpo C/D', '');
-insert into `destinations` values (0, 255, 'UE4', 'Corpo A/B', '');
-insert into `destinations` values (0, 255, 'UE6', '');
+insert into `picking_points` values (0, 255, 'UE5 - P0', '');
+insert into `picking_points` values (0, 255, 'UE5 - P1', '');
+insert into `picking_points` values (0, 255, 'UE5 - P2', '');
+insert into `picking_points` values (0, 255, 'UE5 - P3', '');
+insert into `picking_points` values (0, 255, 'UE5 - P4', '');
+insert into `picking_points` values (0, 255, 'UE5 - P5', '');
+insert into `picking_points` values (0, 255, 'UE5 - P6', '');
+insert into `picking_points` values (0, 255, 'UE5 - P7', '');
+insert into `picking_points` values (0, 255, 'UE4 - Corpo C/D', '');
+insert into `picking_points` values (0, 255, 'UE4 - Corpo A/B', '');
+insert into `picking_points` values (0, 255, 'UE6', '');
 
 ALTER TABLE operations
-ADD COLUMN destination_id INT(11),
-ADD CONSTRAINT fk_operation_destination
-    FOREIGN KEY (destination_id)
-    REFERENCES destinations(id)
+ADD COLUMN picking_point_id INT(11),
+ADD CONSTRAINT fk_operation_picking_point
+    FOREIGN KEY (picking_point_id)
+    REFERENCES picking_points(id)
     ON DELETE SET NULL
     ON UPDATE CASCADE;
 
 ALTER TABLE delegations
-ADD COLUMN destination_id INT(11),
-ADD CONSTRAINT fk_delegation_destination
-    FOREIGN KEY (destination_id)
-    REFERENCES destinations(id)
+ADD COLUMN picking_point_id INT(11),
+ADD CONSTRAINT fk_delegation_picking_point
+    FOREIGN KEY (picking_point_id)
+    REFERENCES picking_points(id)
     ON DELETE SET NULL
     ON UPDATE CASCADE;
 
