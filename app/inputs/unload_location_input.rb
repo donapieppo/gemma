647ab6fb063040b577fb202084b09946b0d49f52
Pietro Donatini <pietro.donatini@unibo.it>
2025-12-11 13:15:52 +0100

    disabled and style

--- app/inputs/unload_location_input.rb
+++ app/inputs/unload_location_input.rb
@@ -2,9 +2,12 @@ class UnloadLocationInput < SimpleForm::Inputs::Base
   def input(wrapper_options)
     res = ""
     object.thing.deposits.each do |dep|
-      disabled = (dep.actual < 1) ? 'disabled="disabled"' : ""
-      res += %|<input id="id_#{dep.id}" type="radio" value="#{dep.id}" name="unload[deposit_id]" #{disabled}></input>
-               <label for="id_#{dep.id}">#{dep.location} (#{dep.actual} disponibili)</label><br/>|.html_safe
+      input_disabled = (dep.actual < 1) ? 'disabled="disabled"' : ""
+      label_disabled = (dep.actual < 1) ? 'class="text-muted"' : ""
+      res += %|
+        <input id="id_#{dep.id}" type="radio" value="#{dep.id}" name="unload[deposit_id]" #{input_disabled}></input>
+        <label for="id_#{dep.id}" #{label_disabled}>#{dep.location} (#{dep.actual} disponibili)</label><br/>
+        |.html_safe
     end
     res
   end
