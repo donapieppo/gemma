01b38d0fb697e8a925be803bd9581791bdd14723
Pietro Donatini <pietro.donatini@unibo.it>
2026-01-16 12:34:23 +0100

    booking for @user.authorization.can_only_book?(@user.current_organization)

--- app/policies/organization_policy.rb
+++ app/policies/organization_policy.rb
@@ -2,16 +2,16 @@ class OrganizationPolicy < DmUniboCommon::OrganizationPolicy
   configure_authlevels
 
   def index?
-    @user.is_cesia?
+    @user&.is_cesia?
   end
 
   # user in organizations with booking can read
   def read?
-    @user && @user.current_organization && (@user.authorization.can_read?(@user.current_organization) || @user.current_organization.booking)
+    @user&.current_organization && (@user.authorization.can_read?(@user.current_organization) || @user.current_organization.booking)
   end
 
   def book?
-    @user && @user.current_organization && @user.current_organization.booking && !@user.authorization.can_only_book?(@user.current_organization)
+    @user&.current_organization&.booking && @user.authorization.can_only_book?(@user.current_organization)
   end
 
   def show?
