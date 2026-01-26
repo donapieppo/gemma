3300a089b03a3cb346d080f5f36d5efe34f81681
Pietro Donatini <pietro.donatini@unibo.it>
2026-01-26 16:44:16 +0100

    changes

--- doc/changes.sql
+++ doc/changes.sql
@@ -1,11 +1,9 @@
-alter table organizations drop with_labs;
-
 -- ALTER TABLE things ADD COLUMN dewars text;
 CREATE TABLE `picking_points` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `organization_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `description` text DEFAULT NULL,
+  `description` TEXT DEFAULT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_picking_points_organizations` FOREIGN KEY (`organization_id`) REFERENCES `organizations` (`id`)
 );
@@ -21,6 +19,20 @@ insert into `picking_points` values (0, 255, 'UE5 - P7', '');
 insert into `picking_points` values (0, 255, 'UE4 - Corpo C/D', '');
 insert into `picking_points` values (0, 255, 'UE4 - Corpo A/B', '');
 insert into `picking_points` values (0, 255, 'UE6', '');
+
+CREATE TABLE cost_centers (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    `organization_id` int(11) NOT NULL,
+    name VARCHAR(200) NOT NULL,
+    description TEXT,
+    CONSTRAINT `fk_cost_center_organizations` FOREIGN KEY (`organization_id`) REFERENCES `organizations` (`id`)
+);
+
+insert into `cost_centers` values (0, 255, 'CILDIC', '');
+insert into `cost_centers` values (0, 255, 'Dipartimento di chimica', '');
+insert into `cost_centers` values (0, 255, 'Dipartimento di chimica industriale', '');
+insert into `cost_centers` values (0, 255, 'Dipartimento Fabit', '');
+
 
 ALTER TABLE operations
 ADD COLUMN picking_point_id INT(11),
@@ -38,19 +50,6 @@ ADD CONSTRAINT fk_delegation_picking_point
     ON DELETE SET NULL
     ON UPDATE CASCADE;
 
-CREATE TABLE cost_centers (
-    id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    `organization_id` int(11) NOT NULL,
-    name VARCHAR(200) NOT NULL,
-    description TEXT,
-    CONSTRAINT `fk_cost_center_organizations` FOREIGN KEY (`organization_id`) REFERENCES `organizations` (`id`)
-);
-
-insert into `cost_centers` values (0, 255, 'CILDIC', '');
-insert into `cost_centers` values (0, 255, 'Dipartimento di chimica', '');
-insert into `cost_centers` values (0, 255, 'Dipartimento di chimica industriale', '');
-insert into `cost_centers` values (0, 255, 'Dipartimento Fabit', '');
-
 ALTER TABLE operations
 ADD COLUMN cost_center_id INT(11),
 ADD CONSTRAINT fk_operation_cost_center
@@ -67,4 +66,6 @@ ADD CONSTRAINT fk_delegation_cost_center
     ON DELETE SET NULL
     ON UPDATE CASCADE;
 
-     
+
+alter table organizations drop with_labs;
+
